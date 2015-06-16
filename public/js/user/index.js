c22ffdfa439d9cb33e87b4f6ca996e5b39a8aeb5
Sachidananda Panigrahi <snpanigrahi88@gmail.com>
2015-06-16 17:23:58 +0530

    working on booking flow

--- public/js/user/index.js
+++ public/js/user/index.js
@@ -1,13 +1,19 @@
-function user(){
+function user() {
 
 }
-user.prototype.staticRoutes = function(){
-    $('#staticRoutes a').on('click', function(){
+user.prototype.staticRoutes = function () {
+    $('#staticRoutes a').on('click', function () {
         localStorage.setItem("city_from", $(this).parents('tr')[0].children[1].innerText);
         localStorage.setItem("city_to", $(this).parents('tr')[0].children[3].innerText);
-    })
+        $('.selectRoute .from').val(localStorage.getItem("city_from"));
+        $('.selectRoute .to').val(localStorage.getItem("city_to"));
+    });
+    if (localStorage.getItem("city_from") !== null && localStorage.getItem("city_from") !== '' && localStorage.getItem("city_to") !== null && localStorage.getItem("city_to") !== '') {
+        $('.selectRoute .from').val(localStorage.getItem("city_from"));
+        $('.selectRoute .to').val(localStorage.getItem("city_to"));
+    }
 };
-$(document).ready(function(){
+$(document).ready(function () {
     validation();
     var newUserObj = new user();
     newUserObj.staticRoutes();
